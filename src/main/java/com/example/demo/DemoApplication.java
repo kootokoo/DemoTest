a6206db53c055585d429b372c48966027a9028e2
Joy <kootokoo@gmail.com>
2018-02-25 18:38:32 +0900

    Revert "test2"
    This reverts commit 647fd74977f3a4a475817b7f838c25312bad43c5.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		int a = 1;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
