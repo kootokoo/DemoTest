eada3e264c9db382592ddbdca5ebfc0fbb22db85
Joy <kootokoo@gmail.com>
2018-02-25 18:12:23 +0900

    test3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		int a = 1;
+		int a = 2;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
